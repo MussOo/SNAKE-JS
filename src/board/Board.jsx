58a555187e94c62ebabbb3b71d049ca64f9b4707
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2024-09-13 10:41:27 +0200

    set board style

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+import './Board.css';
 
+const MAP_SIZE = 20;
 
 export default function Board() {
+    const [board, setBoard] = useState(
+        Array(MAP_SIZE).fill(Array(MAP_SIZE).fill(0))
+    );
+
 
     return (
-        <div>
+        <div className='main'>
         <h1>Board</h1>
+        <div className="board">
+            {board.map((row, i) => (
+                <div key={i} className="row">
+                    {row.map((cell, j) => (
+                        <div key={j} className="cell"></div>
+                    ))}
+                </div>
+            ))}
+        </div>
         </div>
     )
 }
